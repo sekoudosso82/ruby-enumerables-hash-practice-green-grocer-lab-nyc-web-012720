fcf62bc4a52574a7b6a65efc1dbd0de5f9df918b
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-05 18:52:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def apply_clearance(cart)
 end 
 
 def checkout(array, coupons)
-    # hash_cart = consolidate_cart(array)
+    hash_cart = consolidate_cart(array)
     # applied_coupons = apply_coupons(hash_cart, coupons)
     # appleid_discount  = apply_clearance(applied_coupons)
     # total = appleid_discount.reduce(0){| acc, {key, value}| acc += value[:price]*value[:count] }
