13ee0ba8c7d4067e51690889e4ccec3d9f9b0f80
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-05 18:42:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,6 +48,11 @@ def apply_clearance(cart)
 
 end 
 
-def checkout(cart, coupons)
-  # code here
-end
+def checkout(array, coupons)
+    hash_cart = consolidate_cart(array)
+    applied_coupons = apply_coupons(hash_cart, coupons)
+    appleid_discount  = apply_clearance(applied_coupons)
+    total = appleid_discount.reduce(0){|acc, {key, value}| acc += value[:price]*value[:count]}
+    # tenary operator
+    total>100? total*0.9 : total 
+end 
